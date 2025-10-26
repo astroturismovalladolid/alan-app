06157c4d8a5aaaf19526f56fa99ff7b38387e6b2
Riru Losa <losariru55788@gmail.com>
2025-10-26 12:07:42 +0000

    Try fixing this error: `Runtime Error: Error: Map container is already i

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,38 +1,54 @@
 'use client';
 
+import React, { useEffect, useRef, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import React from 'react';
 
 // Fix for default icon path in Leaflet with webpack
 if (typeof window !== 'undefined') {
   delete (L.Icon.Default.prototype as any)._getIconUrl;
 
   L.Icon.Default.mergeOptions({
-    iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+    iconRetinaUrl:
+      'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
     iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    shadowUrl:
+      'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
   });
 }
 
 function MapComponent() {
   const position: [number, number] = [51.505, -0.09]; // Default position
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
 
   return (
-    <MapContainer center={position} zoom={13} style={{ height: '100%', width: '100%', zIndex: 0 }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {/* You can map over real data to create markers */}
-      {/* Example with a single marker */}
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <>
+      {isClient && (
+        <MapContainer
+          center={position}
+          zoom={13}
+          style={{ height: '100%', width: '100%', zIndex: 0 }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {/* You can map over real data to create markers */}
+          {/* Example with a single marker */}
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      )}
+    </>
   );
 }
 
